3924e769dc29271a5dfdc47b7e19ac0638a5e051
Jooyeon Park <jooyepar@c2r10s3.42gyeongsan.kr>
2026-08-12 18:55:55 +0900

    fix(cmds) : NICKNAME 패킷 전송

--- src/parser/commands/Nick.cpp
+++ src/parser/commands/Nick.cpp
@@ -5,6 +5,7 @@
 #include "common/Replies.hpp"
 #include "common/Utils.hpp"
 
+#include <iostream>
 
 Nick::Nick() {}
 Nick::Nick(const Nick& other) : ICommand(other) {}
@@ -42,8 +43,14 @@ void Nick::execute(Server& server, Client& client, const Message& msg)
         client.appendToOutBuffer(reply(Numeric::ERR_NICKNAMEINUSE, target, nickname + " :Nickname is already in use"));
         return;
     }
+
+    //추가
+    std::string oldNick = client.getNickname();
+
     // 닉네임 설정 (m_clients가 single source of truth이므로 client.setNickname만으로 서버 전체에 즉시 반영)
     client.setNickname(nickname);
+
+    std::cout << "nickname 세팅" << nickname << std::endl;
 
     // 등록 부분
     // 등록되지 않았고, 클라이언트의 올바를 비밀번호이며, 유저 정보가 있다면.
@@ -52,5 +59,12 @@ void Nick::execute(Server& server, Client& client, const Message& msg)
         client.setRegistered(true); // 클라이언트 등록
         client.appendToOutBuffer(reply(Numeric::RPL_WELCOME, nickname, ":Welcome to the IRC network, " + nickname));
         // 환영해요.
+    }
+
+    // 이미 등록된 클라이언트가 닉네임을 변경한 경우, NICK 변경 패킷 알림 전송
+    if (client.isRegistered() && !oldNick.empty())
+    {
+        std::string nickMsg = ":" + oldNick + "!" + client.getUsername() + "@" + client.getIp() + " NICK :" + nickname + "\r\n";
+        client.appendToOutBuffer(nickMsg);
     }
 }
