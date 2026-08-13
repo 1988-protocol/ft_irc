1e9e167d729147b1fbd33102de431c8d822e27b4
jojuma1325 <johminjeong@naver.com>
2026-08-14 08:23:54 +0900

    fix(channel): 주석정리

--- src/channel/Channel.cpp
+++ src/channel/Channel.cpp
@@ -11,14 +11,9 @@ std::string Channel::getName() const
     return m_name;
 }
 
-std::string Channel::getTopic() const
+const std::map<Client*, bool>& Channel::getMembers() const
 {
-    return m_topic;
-}
-
-std::string Channel::getKey() const
-{
-    return m_key;
+    return m_members;
 }
 
 // 현재 설정된 모드 상태를 문자열로 반환
@@ -29,21 +24,12 @@ std::string Channel::getModeString() const
     if (m_isTopicOp) modes += "t";
     if (!m_key.empty()) modes += "k";
     if (m_userLimit > 0) modes += "l";
-    
-    return (modes == "+") ? "" : modes;
+    if (modes == "+")
+        return "";
+    return modes;
 }
 
-const std::map<Client*, bool>& Channel::getMembers() const
-{
-    return m_members;
-}
-
-void Channel::setTopic(std::string topic)
-{
-    m_topic = topic;
-}
-
-// --- Member Management ---
+// 채널 Member 관리
 
 void Channel::addMember(Client* client)
 {
@@ -66,7 +52,17 @@ bool Channel::isMember(Client* client) const
     return m_members.count(client) > 0;
 }
 
-// --- Key (+k) ---
+// 채널 Key 관리
+
+std::string Channel::getKey() const
+{
+    return m_key;
+}
+
+void Channel::setKey(const std::string& key)
+{
+    m_key = key;
+}
 
 bool Channel::isKeyModeActive() const
 {
@@ -78,17 +74,12 @@ bool Channel::checkKey(const std::string& key) const
     return m_key == key;
 }
 
-void Channel::setKey(const std::string& key)
-{
-    m_key = key;
-}
-
 void Channel::removeKey()
 {
     m_key = "";
 }
 
-// --- Invite Only (+i) ---
+// 채널 Invite 관리
 
 bool Channel::isInviteOnly() const
 {
@@ -122,7 +113,17 @@ void Channel::removeInvite(Client* client)
     m_invitedUsers.erase(client);
 }
 
-// --- Topic Restriction (+t) ---
+// 채널 topic 관리
+
+std::string Channel::getTopic() const
+{
+    return m_topic;
+}
+
+void Channel::setTopic(std::string topic)
+{
+    m_topic = topic;
+}
 
 bool Channel::isTopicOpOnly() const
 {
@@ -134,7 +135,7 @@ void Channel::setTopicOpOnly(bool flag)
     m_isTopicOp = flag;
 }
 
-// --- User Limit (+l) ---
+// 채널 유저수 관리 (+l)
 
 bool Channel::hasUserLimit() const
 {
@@ -158,13 +159,13 @@ void Channel::removeUserLimit()
 
 bool Channel::isFull() const
 {
-    if (!hasUserLimit()) // 인원 제한 모드가 꺼져있으면 정원 초과일 리 없음
+    if (!hasUserLimit()) // 인원 제한 모드가 꺼져있으면 정원 초과 x
         return false;
         
     return m_members.size() >= static_cast<size_t>(m_userLimit);
 }
 
-// --- Operator (+o) ---
+// 채널 방장 관리
 
 bool Channel::isOperator(Client* client) const
 {
